6f9122a63031e2ab830560620d023f7416099219
pk214 <piyushkrs214@gmail.com>
2024-05-11 14:56:02 +0530

    final commit

--- script.js
+++ script.js
@@ -33,5 +33,91 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// dropdown
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show2");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn2')) {
+  var myDropdown = document.getElementById("myDropdown2");
+    if (myDropdown.classList.contains('show2')) {
+      myDropdown.classList.remove('show2');
+    }
+  }
+}
+
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show3");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn3')) {
+  var myDropdown = document.getElementById("myDropdown3");
+    if (myDropdown.classList.contains('show3')) {
+      myDropdown.classList.remove('show3');
+    }
+  }
+}
+
+function myFunction4() {
+  document.getElementById("myDropdown4").classList.toggle("show4");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn4')) {
+  var myDropdown = document.getElementById("myDropdown4");
+    if (myDropdown.classList.contains('show4')) {
+      myDropdown.classList.remove('show4');
+    }
+  }
+}
+
+function myFunction5() {
+  document.getElementById("myDropdown5").classList.toggle("show5");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn5')) {
+  var myDropdown = document.getElementById("myDropdown5");
+    if (myDropdown.classList.contains('show5')) {
+      myDropdown.classList.remove('show5');
+    }
+  }
+}
+
+function myFunction6() {
+  document.getElementById("myDropdown6").classList.toggle("show6");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn6')) {
+  var myDropdown = document.getElementById("myDropdown6");
+    if (myDropdown.classList.contains('show6')) {
+      myDropdown.classList.remove('show6');
+    }
+  }
 }
